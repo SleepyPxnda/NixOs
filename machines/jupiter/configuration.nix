3533957964089ea75e6c3af992e553de01be5eb2
Finn Markwitz <finn.markwitz@smoothcloud.de>
2023-09-15 09:50:42 +0000

    Added SDDM Dispalymanager

--- machines/jupiter/configuration.nix
+++ machines/jupiter/configuration.nix
@@ -21,6 +21,9 @@
   # services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
 
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+
   # Configure keymap in X11
   services.xserver = {
     layout = "de";
